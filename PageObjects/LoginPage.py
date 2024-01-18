14ad618567e22e6c2df0d4d241f70d94fdd1c78b
ngocchien5596 <buingocchien5596@gmail.com>
2024-01-18 15:29:20 +0700

    write 11 testcase for login

--- PageObjects/LoginPage.py
+++ PageObjects/LoginPage.py
@@ -9,6 +9,7 @@ class LoginPage:
     textbox_email_xpath = "//android.widget.ScrollView/android.view.ViewGroup/android.widget.EditText"
     textbox_pw_xpath = """//android.widget.EditText[@resource-id="RNE__Input__text-input"]"""
     checkbox_agreement_xpath = "//android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup[3]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup"
+    checkbox_agreement_xpath1 = "//android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup[4]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup"
     button_login_xpath = """//android.widget.TextView[@text="Log In"]"""
     icon_reset_xpath = """//android.widget.FrameLayout[@resource-id="android:id/content"]/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup"""
     label_email_xpath = """//android.widget.TextView[@text="Email"]"""
@@ -17,20 +18,36 @@ class LoginPage:
     label_agreement_xpath = """//android.widget.TextView[@text="By using Reset, you agree to our Term of Use, including the arbitration agreement and class action waiver, and acknowledge you have read our Privacy Policy and our Health Checklist."]"""
     label_emailErrorMessage_xpath = """//android.widget.TextView[@text="Please input your email"]"""
     label_pwErrorMessage_xpath = """//android.widget.TextView[@text="Please input your password"]"""
+    label_invalidEmailPW_xpath = """//android.widget.TextView[@text="Invalid Email or Password"]"""
+    button_skip_xpath = """//android.widget.TextView[@text="Skip"]"""
+    label_loggedReset_xpath = """//android.widget.TextView[@text="Reset"]"""
+
     def __init__(self, driver):
             self.driver = driver
 
     def setEmail(self, email):
+        self.driver.find_element(By.XPATH, self.textbox_email_xpath).click()
+        time.sleep(0.5)
         self.driver.find_element(By.XPATH, self.textbox_email_xpath).send_keys(email)
+        time.sleep(0.5)
+        self.driver.hide_keyboard()
         time.sleep(0.5)
 
     def setPW(self, pw):
+        self.driver.find_element(By.XPATH, self.textbox_pw_xpath).click()
+        time.sleep(0.5)
         self.driver.find_element(By.XPATH, self.textbox_pw_xpath).send_keys(pw)
         time.sleep(0.5)
+        self.driver.hide_keyboard()
+        time.sleep(0.5)
 
-    def tapAgrement(self):
+    def tapAgreement(self):
         self.driver.find_element(By.XPATH, self.checkbox_agreement_xpath).click()
+        time.sleep(0.5)
+    def tapAgreement1(self):
+        self.driver.find_element(By.XPATH, self.checkbox_agreement_xpath1).click()
         time.sleep(0.5)
 
     def tapLogin(self):
         self.driver.find_element(By.XPATH, self.button_login_xpath).click()
+        time.sleep(5)
--- TestCases/Login/Login_TC.py
+++ TestCases/Login/Login_TC.py
@@ -10,8 +10,14 @@ from TestCases.conftest import appID
 
 class TestLogin:
 
-    email = "fevex98244@weirby.com"
+    email = "buingocchien5596@gmail.com"
+    invalidEmail = "abc@example.com"
+    spaceAtMiddleEmail = "buingocchien 5596@gmail.com"
+    spaceAtFirstandEndEmail = " buingocchien5596@gmail.com "
     pwd = "Aa@123123"
+    invalidPW = "1111111"
+    spaceAtMiddlePW = "Aa @12313"
+    spaceAtFirstandEnDPW = " Aa@123123 "
     emptyString = ""
     wrongEmail = "abc@gmail.com"
     wrongPw = "123123"
@@ -85,54 +91,269 @@ class TestLogin:
             self.driver.terminate_app(appID)
             assert False
 
-    # Login_02: Check if do not fill both email and password
+    # Login_02: Check if user inputs blank into Email field
     def test_Login_02(self, setup):
-
         self.driver = setup
-        time.sleep(1)
-
-        list_status = []
+        time.sleep(3)
 
         self.lp = LoginPage(self.driver)
 
-        self.lp.tapAgrement()
+        self.lp.setEmail(self.emptyString)
+
+        self.lp.setPW(self.pwd)
+
+        self.lp.tapAgreement1()
 
         self.lp.tapLogin()
 
-        error1 = self.driver.find_element(By.XPATH, LoginPage.label_emailErrorMessage_xpath)
-        error2 = self.driver.find_element(By.XPATH, LoginPage.label_pwErrorMessage_xpath)
+        element = self.driver.find_element(By.XPATH, LoginPage.label_emailErrorMessage_xpath)
 
-        if error1.is_displayed() and error2.is_displayed():
+        if element:
             assert True
             self.driver.terminate_app(appID)
         else:
             self.driver.terminate_app(appID)
             assert False
 
-    # Login_03: Check if do not fill both email and password
-    @pytest.mark.sanity
+    # Login_03: Check if user inputs space at middle of word into Email field
     def test_Login_03(self, setup):
-
         self.driver = setup
-        time.sleep(1)
+        time.sleep(3)
 
-        list_status = []
+        self.lp = LoginPage(self.driver)
+
+        self.lp.setEmail(self.spaceAtMiddleEmail)
+
+        self.lp.setPW(self.pwd)
+
+        self.lp.tapAgreement()
+
+        self.lp.tapLogin()
+
+        element = WebDriverWait(self.driver, 20).until(EC.presence_of_all_elements_located((By.XPATH, LoginPage.label_invalidEmailPW_xpath)))
+
+        if element:
+            assert True
+            self.driver.terminate_app(appID)
+        else:
+            self.driver.terminate_app(appID)
+            assert False
+
+    # Login_04: Check if user inputs space at first and end of word into Email field
+    def test_Login_04(self, setup):
+        self.driver = setup
+        time.sleep(3)
+
+        self.lp = LoginPage(self.driver)
+
+        self.lp.setEmail(self.spaceAtFirstandEndEmail)
+
+        self.lp.setPW(self.pwd)
+
+        self.lp.tapAgreement()
+
+        self.lp.tapLogin()
+
+        element = self.driver.find_elements(By.XPATH, LoginPage.button_skip_xpath)
+        element1 = self.driver.find_elements(By.XPATH, LoginPage.label_loggedReset_xpath)
+
+        if element:
+            assert True
+            self.driver.terminate_app(appID)
+        elif element1:
+            assert True
+            self.driver.terminate_app(appID)
+        else:
+            self.driver.terminate_app(appID)
+            assert False
+
+
+    # Login_05: Check if user input invalid email into Email field
+    def test_Login_05(self, setup):
+        self.driver = setup
+        time.sleep(3)
+
+        self.lp = LoginPage(self.driver)
+
+        self.lp.setEmail(self.invalidEmail)
+
+        self.lp.setPW(self.pwd)
+
+        self.lp.tapAgreement()
+
+        self.lp.tapLogin()
+
+        element = WebDriverWait(self.driver, 20).until(
+            EC.presence_of_all_elements_located((By.XPATH, LoginPage.label_invalidEmailPW_xpath)))
+
+        if element:
+            assert True
+            self.driver.terminate_app(appID)
+        else:
+            self.driver.terminate_app(appID)
+            assert False
+
+    # Login_06: Check if user input valid email into Email field
+
+    def test_Login_06(self, setup):
+        self.driver = setup
+        time.sleep(3)
 
         self.lp = LoginPage(self.driver)
 
         self.lp.setEmail(self.email)
 
-        self.lp.tapAgrement()
+        self.lp.setPW(self.pwd)
+
+        self.lp.tapAgreement()
 
         self.lp.tapLogin()
 
-        error2 = self.driver.find_element(By.XPATH, LoginPage.label_pwErrorMessage_xpath)
+        element = self.driver.find_elements(By.XPATH, LoginPage.button_skip_xpath)
+        element1 = self.driver.find_elements(By.XPATH, LoginPage.label_loggedReset_xpath)
 
-        if error2.is_displayed():
+        if element:
+            assert True
+            self.driver.terminate_app(appID)
+        elif element1:
             assert True
             self.driver.terminate_app(appID)
         else:
             self.driver.terminate_app(appID)
             assert False
+
+    # Login_07: Check if user inputs blank into Password field
+    def test_Login_07(self, setup):
+        self.driver = setup
+        time.sleep(3)
+
+        self.lp = LoginPage(self.driver)
+
+        self.lp.setEmail(self.email)
+
+        self.lp.setPW(self.emptyString)
+
+        self.lp.tapAgreement()
+
+        self.lp.tapLogin()
+
+        element = self.driver.find_element(By.XPATH, LoginPage.label_pwErrorMessage_xpath)
+
+        if element:
+            assert True
+            self.driver.terminate_app(appID)
+        else:
+            self.driver.terminate_app(appID)
+            assert False
+
+    # Login_08: Check if user inputs space at middle of word into Password field
+    def test_Login_08(self, setup):
+        self.driver = setup
+        time.sleep(3)
+
+        self.lp = LoginPage(self.driver)
+
+        self.lp.setEmail(self.email)
+
+        self.lp.setPW(self.spaceAtMiddlePW)
+
+        self.lp.tapAgreement()
+
+        self.lp.tapLogin()
+
+        element = WebDriverWait(self.driver, 20).until(
+            EC.presence_of_all_elements_located((By.XPATH, LoginPage.label_invalidEmailPW_xpath)))
+
+        if element:
+            assert True
+            self.driver.terminate_app(appID)
+        else:
+            self.driver.terminate_app(appID)
+            assert False
+
+    # Login_09: Check if user inputs space at first and end of word into Password field
+
+    def test_Login_09(self, setup):
+        self.driver = setup
+        time.sleep(3)
+
+        self.lp = LoginPage(self.driver)
+
+        self.lp.setEmail(self.email)
+
+        self.lp.setPW(self.spaceAtFirstandEnDPW)
+
+        self.lp.tapAgreement()
+
+        self.lp.tapLogin()
+
+        element = WebDriverWait(self.driver, 20).until(
+            EC.presence_of_all_elements_located((By.XPATH, LoginPage.label_invalidEmailPW_xpath)))
+
+        if element:
+            assert True
+            self.driver.terminate_app(appID)
+        else:
+            self.driver.terminate_app(appID)
+            assert False
+
+    # Login_10: Check if user input invalid Password into Password field
+    @pytest.mark.sanity
+    def test_Login_10(self, setup):
+        self.driver = setup
+        time.sleep(3)
+
+        self.lp = LoginPage(self.driver)
+
+        self.lp.setEmail(self.email)
+
+        self.lp.setPW(self.invalidPW)
+
+        self.lp.tapAgreement()
+
+        self.lp.tapLogin()
+
+        element = WebDriverWait(self.driver, 20).until(
+            EC.presence_of_all_elements_located((By.XPATH, LoginPage.label_invalidEmailPW_xpath)))
+
+        if element:
+            assert True
+            self.driver.terminate_app(appID)
+        else:
+            self.driver.terminate_app(appID)
+            assert False
+
+
+    # Login_11: Check if user input valid Password into Password field
+    @pytest.mark.sanity
+    def test_Login_11(self, setup):
+        self.driver = setup
+        time.sleep(3)
+
+        self.lp = LoginPage(self.driver)
+
+        self.lp.setEmail(self.email)
+
+        self.lp.setPW(self.pwd)
+
+        self.lp.tapAgreement()
+
+        self.lp.tapLogin()
+
+        element = self.driver.find_elements(By.XPATH, LoginPage.button_skip_xpath)
+        element1 = self.driver.find_elements(By.XPATH, LoginPage.label_loggedReset_xpath)
+
+        if element:
+            assert True
+            self.driver.terminate_app(appID)
+        elif element1:
+            assert True
+            self.driver.terminate_app(appID)
+        else:
+            self.driver.terminate_app(appID)
+            assert False
+
+
+
 
 
